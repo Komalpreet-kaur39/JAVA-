b48fc59f597e508961a4b82ca963cc143d4ef854
Komalpreet Kaur <komalpreet9113@gmail.com>
2024-07-08 13:31:43 +0530

    Image.java

--- Image.java
+++ Image.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Maninder Singh
- */
 public class Image  implements Printable{
 
     private int width,height;
